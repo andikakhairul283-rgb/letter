36de569b65aa8fa80f040c38d5cbce67091bdf15
andikakhairul283-rgb <andikakhairul283@gmail.com>
2025-09-22 10:34:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const message = `Ciee yang udaa maw jadi maba UI hihi,\nicaaa semangaat teruss yaaa cantikk, yukk kita bantai bantai utbk sheess.\n\nabang sebenernyaa bingung mauu ngetik apaa huhu. pokoknya icaa semangat truss, dan hargai prosesnya yeayy\n\nkalauu adekk ngerasa stuck atau lelah, jangan berhenti, ingatt ada abang disini okay? truss cerita sama abang yaa hihi\n\n kita semua bolee capee,\n kitaa boleh bgd ngeluh,\n tapi harus ingettt  \nkita gabole nyerah okay?\n i'll support u.\n \nwith love, Andika.
+`;
+
+function showLetter() {
+  document.getElementById("introText").style.opacity = 0;
+  document.querySelector(".btn").style.display = "none";
+
+  setTimeout(() => {
+    const letterBox = document.getElementById("letterBox");
+    const typedText = document.getElementById("typedText");
+    letterBox.style.display = "block";
+    let i = 0;
+
+    function typeWriter() {
+      if (i < message.length) {
+        typedText.innerHTML += message.charAt(i);
+        i++;
+        setTimeout(typeWriter, 30);
+      }
+    }
+
+    typeWriter();
+  }, 600);
+}
